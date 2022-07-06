4b0cd4ff997e0d51b2f1dfceb5adf0318512738f
Thắng <57650718+Thangvo248@users.noreply.github.com>
2022-07-06 20:53:01 +0700

    update 7/6

--- controllers/vaccine_info.js
+++ controllers/vaccine_info.js
@@ -9,15 +9,17 @@ class vaccine_infoController {
         vaccine_id: vaccine._id,
       }).cache({ time: 30 });
       const data = {
-        preventions: info_vaccine.preventions,
-        subjects: info_vaccine.subjects,
-        contraindications: info_vaccine.contraindications,
-        instructions: info_vaccine.instructions,
-        origin: info_vaccine.origin,
+        vaccineId: vaccine._id,
         name: vaccine.name,
         price: vaccine.price,
         description: vaccine.description,
         available: vaccine.available,
+        preventions: info_vaccine.preventions,
+        subjects: info_vaccine.subjects,
+        contraindications: info_vaccine.contraindications,
+        precautions: info_vaccine.precautions,
+        instructions: info_vaccine.instructions,
+        origin: info_vaccine.origin,
       };
       return res.json({ data: data });
     } catch (err) {
--- models/vaccine_info.js
+++ models/vaccine_info.js
@@ -5,6 +5,7 @@ const vaccine_infoShema = Schema({
   vaccine_id: { type: mongoose.ObjectId },
   preventions: { type: Array, min: 0, max: 100, item: String },
   subjects: { type: Array, min: 0, max: 100, item: String },
+  instructions: { type: Array, min: 0, max: 100, item: String },
   contraindications: { type: Array, min: 0, max: 100, item: String },
   precautions: { type: Array, min: 0, max: 100, item: String },
   origin: { type: String },
